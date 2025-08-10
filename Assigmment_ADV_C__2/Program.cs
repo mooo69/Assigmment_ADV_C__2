3c9abc0326eb6ef609746e217484b6ae875b90ed
mohamed gamal <mohamed.gamalabuy@gmail.com>
2025-08-11 02:01:11 +0300

    q1

--- Assigmment_ADV_C__2/Program.cs
+++ Assigmment_ADV_C__2/Program.cs
@@ -3,32 +3,35 @@
     internal class Program
     {
         #region q1
-       /**/ class CountGreater
-        {
-            public static void Run()
-            {
-                string[] firstLine = Console.ReadLine().Split();
-                int n = int.Parse(firstLine[0]);
-                int q = int.Parse(firstLine[1]);
+        /* class CountGreater
+         {
+             public static void Run()
+             {
+                 string[] firstLine = Console.ReadLine().Split();
+                 int n = int.Parse(firstLine[0]);
+                 int q = int.Parse(firstLine[1]);
 
-                int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+                 int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 
-                for (int i = 0; i < q; i++)
-                {
-                    int x = int.Parse(Console.ReadLine());
-                    int count = 0;
-                    for (int j = 0; j < n; j++)
-                    {
-                        if (arr[j] > x)
-                        {
-                            count++;
-                        }
-                    }
-                    Console.WriteLine(count);
-                }
-            }
-        }
+                 for (int i = 0; i < q; i++)
+                 {
+                     int x = int.Parse(Console.ReadLine());
+                     int count = 0;
+                     for (int j = 0; j < n; j++)
+                     {
+                         if (arr[j] > x)
+                         {
+                             count++;
+                         }
+                     }
+                     Console.WriteLine(count);
+                 }
+             }
+         }*/
         #endregion
+
+       
+
 
     }
 }
