7d5d1edaf718ba1811ac9604e8fc5a6addb7b09c
mohamed gamal <mohamed.gamalabuy@gmail.com>
2025-08-10 22:48:07 +0300

    Add project files.

---
+++ Assigmment_ADV_C__2/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Assigmment_ADV_C__2
+{
+    internal class Program
+    {
+        #region q1
+       /**/ class CountGreater
+        {
+            public static void Run()
+            {
+                string[] firstLine = Console.ReadLine().Split();
+                int n = int.Parse(firstLine[0]);
+                int q = int.Parse(firstLine[1]);
+
+                int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+                for (int i = 0; i < q; i++)
+                {
+                    int x = int.Parse(Console.ReadLine());
+                    int count = 0;
+                    for (int j = 0; j < n; j++)
+                    {
+                        if (arr[j] > x)
+                        {
+                            count++;
+                        }
+                    }
+                    Console.WriteLine(count);
+                }
+            }
+        }
+        #endregion
+
+    }
+}
